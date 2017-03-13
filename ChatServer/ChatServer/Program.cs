781b26036a5ac471e37ffac0d048b9961ce1c338
Christopher Lemelin <clemelin2@gmail.com>
2017-03-13 13:18:27 -0400

    configured nhibernate to connect to a mysql database, the ORM classes made do not reflect what the database will look like, just wanted to test out the connection

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
 
 namespace ChatServer
 {
@@ -7,6 +11,32 @@ namespace ChatServer
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+
+			Product [] myInitialObjects = new Product[2];
+			myInitialObjects[0] = new Product { LastName = "boby", FirstName = "danny" };
+			myInitialObjects[1] = new Product { LastName = "dammy", FirstName = "kacjies" };
+
+
+			var cfg = new NHibernate.Cfg.Configuration();
+			cfg.Configure();
+			// ensure that mapping hbm.xml file is loaded
+			cfg.AddAssembly(typeof(MainClass).Assembly);
+			ISessionFactory factory =
+				cfg.BuildSessionFactory();
+
+			using (ISession session = factory.OpenSession())
+			{
+				// Insert two employees in Database
+				session.Save(myInitialObjects[0]);
+				session.Save(myInitialObjects[1]);
+				session.Flush();
+				//session.Close();
+			}
+
+			Console.WriteLine("Hello World!");
+		
+
+
 			ConnectionListener listener = new ConnectionListener();
 			Model model = new Model();
 			listener.model = model;
