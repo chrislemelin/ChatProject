7a8924582d326b39fb86fb55b4726907789584c7
Christopher Lemelin <clemelin2@gmail.com>
2017-04-22 19:23:33 -0400

    added nHibernate model for rooms

---
+++ ChatServer/ChatServer/nHibernate/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NHibernate;
+namespace ChatServer
+{
+	public class Room
+	{
+
+		public virtual int ID { get; set; }
+
+		public virtual string Title { get; set; }
+
+		public virtual User Owner { get; set; }
+
+		public Room()
+		{
+		}
+	}
+}
